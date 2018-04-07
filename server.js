fe816d967eaa927489b954fce98279e17e6c9e44
Michael Bouwer <michaelbouwer@Michaels-MBP.lan>
2018-04-07 14:02:57 +0200

    more

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const request = require('request')
 const generator = require('./handlers/transaction_generator.js')
 
 setInterval(function () {
-    console.log(generator())
-},1000)
+    //console.log(generator())
+    request('https://x-transaction-handler.herokuapp.com/', (error, response, body) => {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+    })
+}, 3000)
