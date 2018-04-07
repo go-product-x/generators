59bc9d5ff210e9fc41ee43ce26f98c530b1d8caf
Michael Bouwer <michaelbouwer@Michaels-MBP.lan>
2018-04-07 14:30:38 +0200

    fixed url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const generator = require('./handlers/transaction_generator.js')
 
 setInterval(function () {
-    axios.post('https://x-transaction-handler.herokuapp.com//transaction', {
+    axios.post('https://x-transaction-handler.herokuapp.com/transaction', {
         value: generator()
       })
       .then(function (response) {
